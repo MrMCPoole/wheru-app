2565c7c898947520105767ef63a9af75a916c33d
MrMCPoole <mrmcpoole@gmail.com>
2025-08-19 22:02:50 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
-// Minimal fetch handler keeps Chrome happy for installability
-self.addEventListener('fetch', () => {});
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js');
+  });
+}
